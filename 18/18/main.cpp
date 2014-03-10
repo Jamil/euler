4e36a083ae62a631769f25b9e5c66aef97b14691
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-10 15:20:21 -0400

    Problem 18 (Scheme)

---
+++ 18/18/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  18
+//
+//  Created by Jamil Dhanani on 3/6/2014.
+//  Copyright (c) 2014 Jamil Dhanani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
