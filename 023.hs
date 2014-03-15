4f41cc512c7994118aa1d052a42c848d1c051469
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-15 18:04:14 -0400

    Used sieve to determine abundant numbers

--- 023.hs
+++ 023.hs
@@ -1,6 +1,6 @@
-sieve 0 = []
-sieve x = 
-  0 : sieve (x - 1)
+makesieve 0 = []
+makesieve x = 
+  0 : makesieve (x - 1)
 
 marknum n s = 
   (take (n - 1) s) ++ [1]
@@ -8,3 +8,18 @@ marknum n s =
 markmult n s
   | n > (length s) = s
   | otherwise = (marknum n s) ++ (markmult n (drop n s))
+
+sumdivisors n = sum [x | x <- [1..(n-1)], n `mod` x == 0]
+
+arecur :: Int -> [Int] -> [Int]
+arecur n sieve  
+  | null sieve = []
+  | head sieve == 1 = n : (arecur (n + 1) (tail sieve)) 
+  | head sieve == 0 = 
+      if (sumdivisors n > n)
+        then n : (arecur (n + 1) (tail sieve))
+        else (arecur (n + 1) (tail sieve))
+
+abundants :: Int -> [Int]
+abundants x =
+  (arecur 0 (makesieve x))  
