7373713e997ba2091bfec27a554365bc7f666db3
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-16 21:12:18 -0400

    Solved Problem 24 (Haskell)
    List comprehension? Magic????

--- 024.hs
+++ 024.hs
@@ -1,2 +1,7 @@
-permutations :: [Char]
-permutations = [a : b : c : d : e : f : g : h : i : [] | a <- ['0'..'9'], b <- ['0'..'9'], c <- ['0'..'9'], d <- ['0'..'9'], e <- ['0'..'9'], f <- ['0'..'9'], g <- ['0'..'9'], h <- ['0'..'9'], i <- ['0'..'9']] !! 1000000
+import Data.List
+
+-- Permutations from 0 to n
+perms :: [Int] -> [[Int]]
+perms [] = [[]]
+perms n = [x:ps | x <- n, ps <- perms (n \\ [x])]
+
