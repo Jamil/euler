7cfd372acd7faa0ffecf1b1a4d60a1bcba71f837
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-19 07:31:06 -0400

    Wrote checks for recurrences in lists, preliminatry version of solution

--- 026.hs
+++ 026.hs
@@ -1,3 +1,5 @@
+-- List Helper Functions
+-- Various functions to help in the processing of lists
 deletelast n [] = []
 deletelast 0 x = x
 deletelast n x = deletelast (n - 1) (init x)
@@ -5,6 +7,8 @@ deletelast n x = deletelast (n - 1) (init x)
 takelast n x = 
   x2
   where (x1, x2) = splitAt (length x - n) x
+
+-- Repeater Functions
 
 rep [] = []
 rep x = repsequence x []
@@ -14,16 +18,35 @@ repsequence x y
   | (length y > length x) = []
   | otherwise =
       if ((takelast (length y) x) == y)
-        then y
+        then max y (repsequence (init x) (last x : y))
         else repsequence (init x) (last x : y)
 
-makelist :: (RealFrac a) => a -> [Int]
-makelist f 
-  | f < 1 = makelist (f * 10)
-  | fromIntegral (floor f) == f = []
-  | otherwise = (mod (floor f) (10 :: Int)) : makelist (f * 10)
+-- does y repeat in all x?
+doesrepeat [] y = True
+doesrepeat x y 
+  | (length x < (length y)) = False
+  | otherwise = 
+      if ((takelast (length y) x) == y)
+        then (doesrepeat (deletelast (length y) x) y)
+        else False
 
-longest :: Int -> [(Int, Int)]
-longest 0 = [(0,0)]
-longest n = 
-  ((length (rep (makelist (1/((fromIntegral n) :: Double))))), n) : (longest (n - 1))
+-- Decomposes a list into its smallest repeating element (if any)
+-- Try first n elements of x
+atomize [] n = []
+atomize x n =
+  if (doesrepeat x (take n x))
+    then (take n x)
+    else atomize x (n + 1)
+
+declist :: (Integral a) => a -> [a]
+declist 0 = [];
+declist x = (declist (div x 10)) ++ [mod x 10]
+
+fullproc n = atomize (rep (declist $ bigdiv n)) 1
+
+bigdiv n = (div (10^1000) n) 
+
+-- Generation
+longest 1 = []
+longest n = ((length (fullproc n)), n) : (longest (n - 1))
+  
