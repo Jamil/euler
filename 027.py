b8fe7b75063780221254ab5b304a8135f32d86a3
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-20 08:47:34 -0400

    Started Problem 27 (Python)

--- 027.py
+++ 027.py
@@ -1,30 +1,40 @@
+import math
+import array
+
 def markSieve(n,sieve):
-  for i in (1, len(sieve)):
-    if (i % n == 0):
+  for i in range(1, len(sieve) - 1):
+    if i % n == 0:
       sieve[i] = 1
+  return sieve
 
 def isPrime(n):
-  for i in (1, sqrt(n)):
-    if (n % i == 0):
+  for i in range(2, int(math.sqrt(n) + 1)):
+    if n % i == 0:
       return False
   return True
 
 def findPrimes(n):
   primes = []
-  sieve = [0] * n
-  for i in (1,n):
-    if (isPrime(i)):
+  sieve = [0]*n
+  sieve[1] = 1
+  for i in range(2, len(sieve)):
+    if isPrime(i):
+      sieve = markSieve(i, sieve)
       primes.append(i)
   return primes
 
+def tryQuadratic(a,b,c):
+  allprimes = True
+  x = 0
+  while allprimes:
+    
+
 def quadprimes(n):
   maxNum = 0
-  for i in (-999,999):
-    for j in (-999,999):
+  for i in range(-999,999):
+    for j in range(-999,999):
       num = tryQuadratic(i,i,j)
-      if (num > maxNum):
+      if num > maxNum:
         maxNum = num
 
-def main():
-  print "Hello"
-  print findPrimes(10)
+print findPrimes(100)
