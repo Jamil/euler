a945ff9d62f017cfe58add5cb440443ab15c6f3e
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-24 15:15:51 -0400

    Memoized version

--- 031.hs
+++ 031.hs
@@ -1,10 +1,17 @@
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-coinsums 0 = [[]]
-coinsums n = [x:ps | x <- coins, n >= x, ps <- (coinsums (n - x))]
+fix n = removedups $ organize n
+
+coinsums = (map coinsums' [0..] !!)
+  where
+    coinsums' 0 = [[]]
+    coinsums' n = [x:ps | x <- coins, n >= x, ps <- (coinsums (n - x))]
 
 organize [] = []
 organize (x:xs) = [(length $ filter (== p) x) | p <- coins] : (organize xs)
