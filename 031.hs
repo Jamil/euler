a52429289162fd8603a9a31e3eae788e9a9ad59b
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-26 09:14:44 -0400

    Retrying Problem 31 with tuples (Haskell)

--- 031.hs
+++ 031.hs
@@ -1,17 +1,10 @@
-coins = [200, 100, 50, 20, 10, 5, 2, 1]
+emptyWallet = [(0, 200), (0, 100), (0, 50), (0, 20), (0, 10), (0, 5), (0, 2), (0, 1)]
 
-coinsums n = (maxSum n) : distributeAll (maxSum n)
+-- Distribute carry over the list xs
+distribute [] carry = []
+distribute (x:xs) carry = 
+  balance + (carry `div` denomination) : distribute xs (carry `mod` denomination) 
+    where 
+      denomination = snd x
+      balance = fst x
 
-maxSum n = distribute [0,0,0,0,0,0,0,0] 0 n
-
-distribute xs 8 carry = xs
-distribute (x:xs) i carry =
-  x + (div carry (coins !! i)) : distribute xs (i + 1) (mod carry (coins !! i))
-
-distributeFirst (x:xs) i
-  | x == 0 = x : distributeFirst xs (i + 1)
-  | otherwise = (x - 1) : distribute xs (i + 1) (coins !! i)
-
-distributeAll xs 
-  | (all (== 0) (take (length coins - 1) xs)) = []
-  | otherwise = distributeFirst xs 0 : (distributeAll (distributeFirst xs 0))
