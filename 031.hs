7f6d9adbaa7db821a629b59660ac82271eae76fb
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-26 12:00:52 -0400

    Changed arg order

--- 031.hs
+++ 031.hs
@@ -1,9 +1,9 @@
 emptyWallet = [(0, 200), (0, 100), (0, 50), (0, 20), (0, 10), (0, 5), (0, 2), (0, 1)]
 
 -- Distribute carry over the list xs
-distribute [] carry = []
-distribute (x:xs) carry = 
-  balance + (carry `div` denomination) : distribute xs (carry `mod` denomination) 
+distribute carry [] = []
+distribute carry (x:xs) = 
+  balance + (carry `div` denomination) : distribute (carry `mod` denomination) xs
     where 
       denomination = snd x
       balance = fst x
