1d75a4ab2762e0605658483b3c9f27efcfaf9e4e
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-26 14:08:58 -0400

    Working on Problem 31

--- 031.hs
+++ 031.hs
@@ -1,10 +1,37 @@
 emptyWallet = [(0, 200), (0, 100), (0, 50), (0, 20), (0, 10), (0, 5), (0, 2), (0, 1)]
 
--- Distribute carry over the list xs
+-- Distribute carry over the list xs 
 distribute carry [] = []
 distribute carry (x:xs) = 
-  balance + (carry `div` denomination) : distribute (carry `mod` denomination) xs
+  (balance + (carry `div` denomination), denomination) : distribute (carry `mod` denomination) xs
     where 
       denomination = snd x
       balance = fst x
+
+-- Distributes one coin of denomination 'n' over the lower denominations 
+distributeCoin n [] = [] -- Not found; should never happen
+distributeCoin n (x:xs)
+  | n == denomination && balance > 0 = 
+      ((balance - 1), denomination) : distribute denomination xs
+  | otherwise = 
+      x : distributeCoin n xs
+  where 
+    denomination = snd x
+    balance = fst x
+
+decomposition n = distributeCoin n $ distribute n emptyWallet
+
+distributeEach xs 8 = []
+distributeEach xs i  
+  | balance /= 0 = distributeCoin denomination xs : distributeEach xs (i + 1)
+  | otherwise = distributeEach xs (i + 1)
+  where 
+    denomination = snd (xs !! i)
+    balance = fst (xs !! i)
+
+addLists [] [] = []
+addLists (a:as) (b:bs) = a + b : addLists as bs 
+
+-- Only show first element of tuple
+f = map fst
 
